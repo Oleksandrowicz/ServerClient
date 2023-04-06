e3a1ef1583b1de14c1defeb00f4c547846532c08
Oleksandrowicz <miroslav28321@gmail.com>
2023-04-06 17:35:04 +0300

    Add project files.

---
+++ ServerClient/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ServerClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static int port = 8080;
+        static string address = "127.0.0.1";
+        string text = "";
+        string filename = "";
+        public MainWindow()
+        {
+
+            InitializeComponent();
+        }
+        IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(address), port);
+
+         TcpClient client = new TcpClient();
+
+        
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            
+            if (openFileDialog1.ShowDialog() == true) // Test result.
+            {
+                string file = openFileDialog1.FileName;
+                try
+                {
+                    string text = File.ReadAllText(file);
+                    filename = openFileDialog1.FileName;
+                }
+                catch (IOException)
+                {
+                }
+            }
+            client.Connect(ipPoint);
+        }
+    }
+}
---
+++ tcp_server/Program.cs
@@ -0,0 +1,43 @@
+﻿using SharedData;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace tcp_server
+{
+    internal class Program
+    {
+        static int port = 8080;
+        static void Main(string[] args)
+        {
+            IPAddress iPAddress = IPAddress.Parse("127.0.0.1");//Dns.GetHostEntry("localhost").AddressList[1]; //localhost
+            IPEndPoint ipPoint = new IPEndPoint(iPAddress, port);
+
+            TcpListener listener = new TcpListener(ipPoint); // bind
+            listener.Start(10);
+            while (true)
+            {
+                Console.WriteLine("Server started, waiting for connection");
+                TcpClient client = new TcpClient();
+                try
+                {
+                    while (client.Connected)
+                    {
+                        NetworkStream ns = client.GetStream();
+                        BinaryFormatter formatter = new BinaryFormatter();
+                        var checkfile = (CheckFile)formatter.Deserialize(ns);
+                        
+                    }
+                    client.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Client finished with work");
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            listener.Stop();
+        }
+    }
+}
